b512dc25b248418014b2ec73dabb0370084b4f40
philippealexandremunch@gmail.com <Moustache91360>
2020-03-18 21:16:05 +0100

    beta

--- dependency/apt_tools.sh
+++ dependency/apt_tools.sh
@@ -1,1 +1,7 @@
 sudo apt install whois
+sudo apt update
+echo "[*] Need API Key To InterAct With VirusTotal [*]"
+echo "create an account: https://www.virustotal.com/gui/join-us"
+echo "Get API KEY: https://support.virustotal.com/hc/en-us/articles/115002088769-Please-give-me-an-API-key"
+echo " "
+./vt init
