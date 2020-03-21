61fe4b419cbdec5ba34022435daa95d07a9cbbd9
philippealexandremunch@gmail.com <Moustache91360>
2020-03-21 13:00:53 +0100

    Version: 1.1.2

--- dependency/apt_tools.sh
+++ dependency/apt_tools.sh
@@ -13,5 +13,5 @@ echo -e "\e[92m create an account:\e[34m https://www.virustotal.com/gui/join-us"
 echo " "
 echo -e "\e[92m Get API KEY:\e[34m https://support.virustotal.com/hc/en-us/articles/115002088769-Please-give-me-an-API-key"
 echo " "
-echo " "
+echo -e "\e[39m "
 ./dependency/vt init
