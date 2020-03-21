6916eda7d861fdfdbefe564643a6b950df203be9
philippealexandremunch@gmail.com <Moustache91360>
2020-03-21 01:41:22 +0100

    Version: 1.1.2

--- dependency/apt_tools.sh
+++ dependency/apt_tools.sh
@@ -2,11 +2,16 @@
 sudo apt install whois
 sudo apt install exiftool
 sudo apt update
-echo "[*] Need API Key To InterAct With VirusTotal [*]"
+echo " "
+echo " ---------------------------------------------------"
 echo " "
 echo " "
-echo "create an account: https://www.virustotal.com/gui/join-us"
-echo "Get API KEY: https://support.virustotal.com/hc/en-us/articles/115002088769-Please-give-me-an-API-key"
+echo -e "\e[33m [*] Need API Key To InterAct With VirusTotal [*]"
+echo " "
+echo " "
+echo -e "\e[92m create an account:\e[34m https://www.virustotal.com/gui/join-us"
+echo " "
+echo -e "\e[92m Get API KEY:\e[34m https://support.virustotal.com/hc/en-us/articles/115002088769-Please-give-me-an-API-key"
 echo " "
 echo " "
 ./dependency/vt init
