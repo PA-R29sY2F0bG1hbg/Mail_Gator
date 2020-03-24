d93e096d872d40f265e031ba0daf1cf4417d0164
philippealexandremunch@gmail.com <Moustache91360>
2020-03-24 18:33:12 +0100

    Update auto_analyse.py

--- functions/auto_analyse.py
+++ functions/auto_analyse.py
@@ -15,8 +15,8 @@ from functions import email_identities
 def auto_analyse_full(file):
     contenu = email_identities.all_meta_data_var(file)
     date = datetime.datetime.now()
-    name_file = "file_analyse" + str(date) + ".txt"
-    file_analyse = open("Repport.txt", "w")
+    name_file = "Report" + " " + str(date) + ".txt"
+    file_analyse = open(name_file, "w")
     file_analyse.write("test lalalala \n")
     file_analyse.write(contenu)
     file_analyse.close()
