82eb85e24f0124ed6089553e72b65bfd2e461fa8
Narufury <61327682+Narufury@users.noreply.github.com>
2020-03-26 22:05:35 +0100

    Update auto_analyse.py

--- functions/auto_analyse.py
+++ functions/auto_analyse.py
@@ -1,26 +1,24 @@
 """"
-	Auteur : Julien Michelet
+	Auteur : Julien Michelet et Philippe-Alexandre Munch
 	Date : mars 2020
 	But du programme :
 	Fonction automatique des fonctions suivantes :
 
-	Version : 0.0
+	Version : 1.0
 """
-import os
-import sys
-import time
+
 from datetime import datetime
 from functions import email_identities
+from functions.functions_basique import search_word_add_string, read_file
 
 today_date = datetime.now()
-report_date = "\nDATE: ", str(today_date.month) + str(today_date.day) + str(today_date.year) \
-              + str(today_date.hour) + str(today_date.minute)
 
 
 def write_file(name_file, contenu, choix_write):
     file_analyse = open(name_file, choix_write)
     file_analyse.write(contenu)
     file_analyse.close()
+
 
 def write_file_tableau(name_file, contenu, choix_write):
     file_analyse = open(name_file, choix_write)
@@ -30,28 +28,33 @@ def write_file_tableau(name_file, contenu, choix_write):
 
 
 def read_file_write_report(report_header, name_file, choix_write):
-    file_header = open("./graphical/report_header.py", "r")
+    file_header = open(report_header, "r")
     contenu_header = file_header.read()
-    write_file(name_file, contenu_header, choix_write)
     file_header.close()
+    write_file(name_file, contenu_header, choix_write)
 
 
 def auto_analyse_full(file):
     # Report + Report Head
+    day_date = str(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
     today_date = datetime.now()
     name_file = "Report" + "_" + str(today_date.month) + "-" + str(today_date.day) + \
                 "-" + str(today_date.year) + "-" + str(today_date.hour) + \
                 ":" + str(today_date.minute) + ".txt"
 
-    # Report Header
+        # Report Header, creation ou suppression du fichier existant
     read_file_write_report("./graphical/report_header.py", name_file, "w")
+    contenu = search_word_add_string(str(read_file(name_file)), "Report:", name_file)
+    contenu = search_word_add_string(contenu, "Date:", day_date)
+    write_file(name_file, contenu, "w")
 
-    # full meta data
-    contenu = email_identities.all_meta_data_var(file)
-    write_file(name_file, contenu, "a")
+        # full meta data - pas d'utilité à voir plus tard
+    # contenu = email_identities.all_meta_data_var(file)
+    # write_file(name_file, contenu, "a")
 
     # Short Meta data
-    email_identities.short_meta_data_var(file, name_file)
+    contenu = email_identities.short_meta_data_var(file)
+    write_file_tableau(name_file, contenu, "a")
 
     # body extract
     cont_tab = ["\n==========================",
