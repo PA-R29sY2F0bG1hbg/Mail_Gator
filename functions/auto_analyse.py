59cfbad6e43630bac3af48d0c7cef076ecc1ead5
Narufury <61327682+Narufury@users.noreply.github.com>
2020-03-26 22:17:01 +0100

    Update auto_analyse.py

--- functions/auto_analyse.py
+++ functions/auto_analyse.py
@@ -9,29 +9,7 @@
 
 from datetime import datetime
 from functions import email_identities
-from functions.functions_basique import search_word_add_string, read_file
-
-today_date = datetime.now()
-
-
-def write_file(name_file, contenu, choix_write):
-    file_analyse = open(name_file, choix_write)
-    file_analyse.write(contenu)
-    file_analyse.close()
-
-
-def write_file_tableau(name_file, contenu, choix_write):
-    file_analyse = open(name_file, choix_write)
-    for i in range(len(contenu)):
-        file_analyse.write(contenu[i])
-    file_analyse.close()
-
-
-def read_file_write_report(report_header, name_file, choix_write):
-    file_header = open(report_header, "r")
-    contenu_header = file_header.read()
-    file_header.close()
-    write_file(name_file, contenu_header, choix_write)
+from functions.functions_basique import search_word_add_string, read_file,write_file, write_file_tableau,read_file_write_report
 
 
 def auto_analyse_full(file):
@@ -52,11 +30,11 @@ def auto_analyse_full(file):
     # contenu = email_identities.all_meta_data_var(file)
     # write_file(name_file, contenu, "a")
 
-    # Short Meta data
+        # Short Meta data
     contenu = email_identities.short_meta_data_var(file)
     write_file_tableau(name_file, contenu, "a")
 
-    # body extract
+        # body extract
     cont_tab = ["\n==========================",
                 "\n[*] Email Body Section [*]",
                 "\n==========================",
