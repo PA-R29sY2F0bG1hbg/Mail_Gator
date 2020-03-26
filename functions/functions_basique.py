0f661b38865ae0fec41fe0c49f602e625876a7dd
Narufury <61327682+Narufury@users.noreply.github.com>
2020-03-26 22:17:57 +0100

    Update functions_basique.py

--- functions/functions_basique.py
+++ functions/functions_basique.py
@@ -22,6 +22,25 @@ def read_file(file):
     return contenu
 
 
+"""Ecriture dans un fichier en passant le nom du fichier, son contenu et type d'écriture ( a, ,w ..)"""
+def write_file(name_file, contenu, choix_write):
+    file_analyse = open(name_file, choix_write)
+    file_analyse.write(contenu)
+    file_analyse.close()
+
+
+def write_file_tableau(name_file, contenu, choix_write):
+    file_analyse = open(name_file, choix_write)
+    for i in range(len(contenu)):
+        file_analyse.write(contenu[i])
+    file_analyse.close()
+
+
+def read_file_write_report(report_header, name_file, choix_write):
+    file_header = open(report_header, "r")
+    contenu_header = file_header.read()
+    file_header.close()
+    write_file(name_file, contenu_header, choix_write)
 """
 report_header = "/root/cadeaux_destines_aux_clients_de_LiDL..eml"
 file_header = open(report_header, "r")
